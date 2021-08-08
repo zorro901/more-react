064731a0a858278b8f4bb2113f54596aeef7d53a
zorro901 <zoro907@gmail.com>
2021-08-08 16:40:32 +0900

    11. おまけ(useMemo)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState ,useCallback} from "react";
+import { useState ,useCallback, useMemo} from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -15,6 +15,8 @@ export default function App() {
   const onClickOpen = () => setOpen(!open);
   const onClickClose = useCallback(() => setOpen(false),[setOpen]);
 
+  const temp = useMemo(()=> 1+ 3,[])
+  console.log(temp);
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
