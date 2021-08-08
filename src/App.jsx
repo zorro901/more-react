3cf7fd3f000bf0fc5a3d60b6740349798e459b89
zorro901 <zoro907@gmail.com>
2021-08-08 16:13:38 +0900

    9. レンダリング最適化１(memo)

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { ChildArea } from "./ChildArea";
+import "./styles.css";
+
+export default function App() {
+  console.log("App");
+  const [text, setText] = useState("");
+  const [open ,setOpen] = useState(false);
+  const countUp = () => {
+    setCount(count + 1);
+  };
+  
+  const onChangeText = (e) => setText(e.target.value);
+
+const onClickOpen = () => setOpen(!open);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open}/>
+    </div>
+  );
+}
