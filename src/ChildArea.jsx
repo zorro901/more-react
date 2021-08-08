e2a9735d0fec809905781eaf961e2f8663a98604
zorro901 <zoro907@gmail.com>
2021-08-08 16:29:20 +0900

    9. レンダリング最適化１(memo)

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,13 +1,12 @@
 import {memo} from "react";
 
-
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor:"khaki"
 }
 
-export const ChildArea = (props) => {
+export const ChildArea = memo((props) => {
   const {open } = props;
   console.log("ChildAreaがレンダリングされた！")
   const data = [...Array(2000).keys()];
@@ -24,4 +23,4 @@ export const ChildArea = (props) => {
     ):null}
     </>
   );
-};
+});
